86f2310e02f84428a9846f4da6120183e366796b
Offiong Bassey <naijaspeed1@gmail.com>
2023-11-24 15:16:46 +0100

    services completed

---
+++ src/server/migrations/20231111150509-create-service.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Services', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      business_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      vendor_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      product_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      product_type_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      status: {
+        type: Sequelize.STRING,
+        defaultValue: "active"
+      },
+      desc: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Services');
+  }
+};
---
+++ src/server/models/service.js
@@ -0,0 +1,33 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Service extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Service.init({
+    business_id: DataTypes.INTEGER,
+    vendor_id: DataTypes.INTEGER,
+    product_id: DataTypes.INTEGER,
+    product_type_id: DataTypes.INTEGER,
+    price: DataTypes.FLOAT,
+    status: 
+    {
+     type: DataTypes.STRING,
+     defaultValue: "active"
+    },
+    desc: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Service',
+  });
+  return Service;
+};
